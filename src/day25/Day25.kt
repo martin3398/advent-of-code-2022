4277dfc11212a997877329ac6df387c64be9529e
Martin Ziegler <martin.ziegler3398@gmail.com>
2022-12-26 17:16:20 +0100

    feat: day 25 improvements

--- src/day25/Day25.kt
+++ src/day25/Day25.kt
@@ -10,20 +10,13 @@ fun main() {
         var snafu = ""
         while (cur > 0) {
             val mod = cur % 5
-
             snafu = when (mod) {
-                0L -> "0"
-                1L -> "1"
-                2L -> "2"
                 3L -> "="
                 4L -> "-"
-                else -> error("not mod 5")
+                else -> mod.toString()
             } + snafu
 
-            cur /= 5
-            if (mod >= 3) {
-                cur++
-            }
+            cur = cur / 5 + mod / 3
         }
 
         return snafu
@@ -31,12 +24,9 @@ fun main() {
 
     fun snafuToDec(snafu: String): Long = snafu.reversed().map {
         when (it) {
-            '2' -> 2L
-            '1' -> 1L
-            '0' -> 0L
             '-' -> -1L
             '=' -> -2L
-            else -> error("not snafu!")
+            else -> it.digitToInt().toLong()
         }
     }.mapIndexed { index, num -> num * 5.0.pow(index).toLong() }.sum()
 
